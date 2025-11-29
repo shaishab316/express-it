ae3766d4f650d96c7129234111417b0e19c040df
shaishab316 <shaishabchandrashil@gmail.com>
2025-11-29 14:16:58 +0600

    chore: Update Prisma config and ignore env files
    Updates Prisma configuration and ignores environment files.
    Modifies the output directory for Prisma client generation and adds
    `.env` to `.gitignore`. Also updates the `prisma-clean` script to
    remove the client directory instead of the entire prisma directory.

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@/../prisma/client.js';
+import { PrismaClient } from '@/../prisma/client/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 import config from '@/config';
-export * from '@/../prisma/client.js';
+export * from '@/../prisma/client/client.js';
 
 /**
  * Prisma Client instance
